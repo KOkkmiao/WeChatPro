a4f0e445628289ffb820f0bf1d45abd33d2cd116
miaoxiaopeng <xiaopeng.miao@1hai.cn>
2020-06-24 18:04:34 +0800

    提交购物车

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,4 +1,9 @@
-// pages/cart/index.js
+/**
+ * 根据淘宝的购物车页面样式来写
+ * 用户打开的时候获取缓存中的数据拉进行商品内容渲染
+ */
+import { request } from "../../request/index.js";
+import runtime from "../../lib/runtime.js";
 Page({
 
   /**
@@ -12,55 +17,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
 
   }
 })
--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -18,30 +18,63 @@ Page({
    */
   onLoad: function (options) {
     const { goods_id } = options;
-    console.log(goods_id);
     this.getGoodsDetail(goods_id);
 
   },
   async getGoodsDetail(goods_id) {
     const result = await request({ url: "/goods/detail", data: { goods_id: goods_id } });
-    console.log(result);
     this.setData({
       goodsDetail: {
         pics: result.pics,
         goods_price: result.goods_price,
         goods_name: result.goods_name,
-        goods_introduce: result.goods_introduce
+        goods_introduce: result.goods_introduce,
+        goods_id: result.goods_id
       }
     });
   },
   prvViewImgeHandle(e) {
     const urls = this.data.goodsDetail.pics.map(v => v.pics_big)
-    const {index}  = e.currentTarget.dataset;
-    console.log(index);
-    
+    const { index } = e.currentTarget.dataset;
     wx.previewImage({
       current: urls[index], // 当前显示图片的http链接
       urls: urls // 需要预览的图片http链接列表
     })
+  },
+  /**
+   * 购物车逻辑
+   * 0用户如果登录进来就去服务器拉取用户对应的购物车信息（Appload中取做）
+   *  1如果未登录则不用管
+   * 1查询本地缓存中是否存在对应的数据
+   * 2如果没有就直接进行添加
+   * 3定时将缓存发到服务器上
+   */
+  handleCartAdd(e) {
+    //1查询本地缓存中是否存在对应的数据
+    let cart = wx.getStorageSync("cart") || [];
+    //如果为空
+    let result = this.data.goodsDetail;
+    let goodsDetail = {
+      pics: result.pics,
+      goods_price: result.goods_price,
+      goods_name: result.goods_name,
+      goods_introduce: result.goods_introduce,
+      goods_id:result.goods_id
+    }
+    let index = cart.findIndex(v => v.goods_id === goodsDetail.goods_id);
+    if (index !== -1) {//表示没有在数据中 直接push进去并替换
+      cart[index].num++;
+    } else {
+      goodsDetail.num = 1;
+      cart.push(goodsDetail);
+    }
+    wx.setStorageSync("cart", cart);
+    wx.showToast({
+      title: '加入成功',
+      icon: 'success',
+      //防手抖
+      mask: true
+    });
   }
+
 })
